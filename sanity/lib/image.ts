4b63b309ab0334d1471d92a48ad1d7303612e263
Apex Growth Management <admin@apexgrowthmanagement.com>
2026-03-05 21:26:51 -0600

    Fix image.ts type import for build

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,9 +1,8 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import { dataset, projectId } from '../env'
 
 const builder = createImageUrlBuilder({ projectId, dataset })
 
-export const urlFor = (source: SanityImageSource) => {
+export const urlFor = (source: Parameters<typeof builder.image>[0]) => {
   return builder.image(source)
 }
